50ee7a5d092aa170c0bb738160fa9d76c4617ef8
Jcmedinah <jean.medina0399@gmail.com>
2026-02-21 11:41:48 -0500

    init: creacio de repositorio base con sus dependencias

---
+++ src/main/java/com/grupo1/inventarioedulend/InventarioedulendApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.inventarioedulend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioedulendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioedulendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1/inventarioedulend/InventarioedulendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.inventarioedulend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioedulendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
